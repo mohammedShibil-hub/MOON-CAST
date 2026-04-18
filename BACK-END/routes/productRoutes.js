9f5e726f273e9f3a02be9b73282b4e74e57ef4a2
Shibil <mohammedshibil12955@gmail.com>
2026-04-18 10:54:02 +0530

    save local changes

--- BACK-END/routes/productRoutes.js
+++ BACK-END/routes/productRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const product = require("../models/productModel")
 
 const {
   getProducts,
@@ -8,5 +9,27 @@ const {
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+
+
+router.get("/similar", async (req, res) => {
+  try {
+    const { brand, category, exclude } = req.query;
+
+    let filter = {
+      _id: { $ne: exclude }
+    };
+
+    if (brand) filter.brand = brand;
+    if (category) filter.category = category;
+
+    const products = await Product.find(filter).limit(6);
+
+    res.json(products);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 module.exports = router;
