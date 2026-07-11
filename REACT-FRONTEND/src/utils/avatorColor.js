dd16339e139ff2a3a08eb6fc18d20f20476e5a6a
Shibil <mohammedshibil12955@gmail.com>
2026-07-11 10:51:17 +0530

    add some color in avatorcolor

--- REACT-FRONTEND/src/utils/avatorColor.js
+++ REACT-FRONTEND/src/utils/avatorColor.js
@@ -2,7 +2,17 @@ export const avatorColor = (name) => {
         const colors = [
             '#ff8974', 
             '#65dd65', 
-            '#60afff', 
+            '#60afff',
+            '#F44336',
+            '#E91E63',
+            '#9C27B0',
+            '#673AB7',
+            '#3F51B5',
+            '#2196F3',
+            '#009688',
+            '#4CAF50',
+            '#FF9800',
+            '#795548',
             '#ce5bff', 
             '#ffb75e'
         ];
